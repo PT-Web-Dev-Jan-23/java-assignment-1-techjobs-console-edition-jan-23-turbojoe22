6066b871fa48fa184e7de8b478a3878de1a28de2
Tom <tpetr00@gmail.com>
2023-04-13 16:16:46 -0500

    tests won't pass, but everything is identical in the comparitor

--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -10,7 +10,7 @@ public class TechJobs {
 
     static Scanner in = new Scanner(System.in);
 
-    public static void main (String[] args) {
+    public static void main(String[] args) {
 
         // Initialize our field map with key/name pairs
         HashMap<String, String> columnChoices = new HashMap<>();
@@ -112,14 +112,24 @@ public class TechJobs {
                 validChoice = true;
             }
 
-        } while(!validChoice);
+        } while (!validChoice);
 
         return choiceKeys[choiceIdx];
     }
 
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
-
-        System.out.println("printJobs is not implemented yet");
+        if (someJobs.size() == 0) {
+            System.out.println("No Results");
+        } else {
+            for (HashMap<String, String> i : someJobs) {
+                System.out.println("\n*****");
+                for (Map.Entry<String, String> j : i.entrySet()) {
+                    System.out.println(j.getKey() + ": " + j.getValue());
+                }
+                System.out.println("*****");
+            }
+            // System.out.println();
+        }
     }
 }
